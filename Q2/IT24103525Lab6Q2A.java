c37bf22d81333f17f2e6b0d922d9f3bb459e5dcb
IT24103525 <it24103525@my.sliit.lk>
2024-08-29 13:13:16 +0530

    Add IT24103525Lab6Q2A.java to Q2

---
+++ Q2/IT24103525Lab6Q2A.java
@@ -0,0 +1,16 @@
+public class IT24103525Lab6Q2A
+{
+    public static void main(String[]args)
+  {      
+
+    int count = 1;
+    while (count<=10)
+     {
+       System.out.print(" "+count);
+
+       count = count+1;
+     }
+
+  }
+
+}
